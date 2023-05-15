f30995fa65dddd5277532f19c5539cef67d4f56a
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-05-16 00:58:10 +0900

    refactor: prettier で reformat しファイル末尾に改行を挿入

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var cardFirst;
 var maxPair = 9;
 var cardImage = [];
 
-window.onload = function(){
+window.onload = function () {
     var arr = [];
- 
-    for (var i = 0; i < maxPair; i++){
+
+    for (var i = 0; i < maxPair; i++) {
         arr.push(i);
         arr.push(i);
     }
- 
+
     shuffle(arr);
- 
+
     var panel = document.getElementById('panel');
-    for (i = 0; i < (maxPair * 2); i++) {
+    for (i = 0; i < maxPair * 2; i++) {
         var div = document.createElement('div');
         div.className = 'card back';
         div.dataset.index = i;
@@ -24,15 +24,15 @@ window.onload = function(){
         panel.appendChild(div);
 
         var img = new Image();
-        img.src = 'images/card'+ (arr[i]+1) +'.png';
+        img.src = 'images/card' + (arr[i] + 1) + '.png';
         cardImage.push(img);
     }
-}
- 
+};
+
 function shuffle(arr) {
     var n = arr.length;
     var temp, i;
- 
+
     while (n) {
         i = Math.floor(Math.random() * n--);
         temp = arr[n];
@@ -42,40 +42,39 @@ function shuffle(arr) {
     return arr;
 }
 
-function turn(e){
+function turn(e) {
     var div = e.currentTarget;
- 
+
     // カードのタイマー処理が動作中
-    if (backTimer)
-        return;
-    
+    if (backTimer) return;
+
     // 裏向きのカードをクリックした場合
     if (div.className == 'card back') {
         div.className = 'card';
         div.appendChild(cardImage[div.dataset.index]);
-    }else{
+    } else {
         // 数字が表示されているカードは return
         return;
     }
- 
+
     // 1枚目の処理
     if (flgFirst) {
         cardFirst = div;
         flgFirst = false;
- 
-    // 2枚目の処理
+
+        // 2枚目の処理
     } else {
         // 数字が1枚目と一致する場合
         if (cardFirst.dataset.number == div.dataset.number) {
-            backTimer = setTimeout(function() {
+            backTimer = setTimeout(function () {
                 div.className = 'card finish';
                 cardFirst.className = 'card finish';
                 backTimer = NaN;
             }, 500);
- 
-        // 一致しない場合
-        } else {  
-            backTimer = setTimeout(function() {
+
+            // 一致しない場合
+        } else {
+            backTimer = setTimeout(function () {
                 div.className = 'card back';
                 div.innerHTML = '';
                 cardFirst.className = 'card back';
@@ -84,7 +83,7 @@ function turn(e){
                 backTimer = NaN;
             }, 500);
         }
- 
+
         flgFirst = true;
     }
 }
